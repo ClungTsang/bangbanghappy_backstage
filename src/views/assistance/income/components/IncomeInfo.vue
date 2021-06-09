ba54359cb64b5ef7481ee9942ee966b20a0e8f3e
ClungTsang <shawn1440982358@163.com>
2021-06-09 23:38:26 +0800

    修复

--- src/views/assistance/income/components/IncomeInfo.vue
+++ src/views/assistance/income/components/IncomeInfo.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <a-descriptions title="" bordered>
+    <a-descriptions title="收益中心" layout="vertical" bordered>
       <a-descriptions-item label="平台总收益" :span="2">
-        {{ data.valuedata }}元
+        {{ income.totalIncome }}元
       </a-descriptions-item>
       <!-- <a-descriptions-item label="押金总金额" :span="2">
         2000
@@ -21,17 +21,18 @@
 export default {
   data() {
     return {
-      data: {},
+      income: {totalIncome:0},
     };
   },
   mounted() {
     this.getRecord();
+    this.getTotalIncome();
   },
   methods: {
     // 获取最新平台总收益
     getTotalIncome() {
       this.$get("/business/rootData").then((res) => {
-        this.data.totalIncome = res.data.data[0].valuedata;
+        this.income.totalIncome = res.data.data[0].valuedata;
       });
     },
     // 获取记录
@@ -40,7 +41,8 @@ export default {
         openid: "manger_bangbanghelper_boss",
       };
       this.$get("/aidDataAnalysis", { ...params }).then((res) => {
-        console.log(res.data.data);
+        // this.income.totalIncome = res.data.data[0].valuedata;
+
         // this.data.value = res.data.data
       });
     },
