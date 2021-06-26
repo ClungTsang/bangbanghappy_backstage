f5d665f70eea5b365f2378e9b6e2f42ac76f2ec6
ClungTsang <shawn1440982358@163.com>
2021-06-27 01:23:05 +0800

    新增模拟订单模块

--- src/views/assistance/assist/Assist.vue
+++ src/views/assistance/assist/Assist.vue
@@ -1,18 +1,37 @@
 <template>
   <a-card :bordered="false">
     <!-- 导出所有订单信息 -->
-    <a-button @click="exportExcel" v-hasPermission="['exportOrder:export']">导出订单表</a-button>
-    <div>
-      <!-- 展示订单信息 -->
-      <assist-category-info></assist-category-info>
-    </div>
+    <a-button
+      @click="exportExcel"
+      type="primary"
+      v-hasPermission="['exportOrder:export']"
+      >导出订单表</a-button
+    >
+    <a-button @click="onOpenMockOrder" v-hasPermission="['mockOrder:add']"
+      >模拟订单</a-button
+    >
+
+    <!-- 展示订单信息 -->
+    <assist-category-info></assist-category-info>
+    <!-- 模拟订单 -->
+    <mock-order-modal
+      :mockVisible="mockVisible"
+      @close="onCloseMockOrder"
+    ></mock-order-modal>
   </a-card>
 </template>
 <script>
 import AssistCategoryInfo from "./components/AssistCategoryInfo.vue";
+import MockOrderModal from "./components/MockOrderModal.vue";
 export default {
+  data() {
+    return {
+      mockVisible: false,
+    };
+  },
   components: {
     AssistCategoryInfo,
+    MockOrderModal,
   },
   methods: {
     exportExcel() {
@@ -26,6 +45,13 @@ export default {
         total = res.data.data.total;
         window.location.href = `https://javabangbanghappy.lanfriend.cn/aidOrder/excel?pageNum=1&pageSize=${total}`;
       });
+    },
+    // 模拟订单modal
+    onOpenMockOrder() {
+      this.mockVisible = true;
+    },
+    onCloseMockOrder() {
+      this.mockVisible = false;
     },
   },
 };
---
+++ src/views/assistance/assist/components/MockOrderModal.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <a-modal
+      :visible="visible"
+      title="新增模拟订单"
+      :destroyOnClose="true"
+      @ok="confirm"
+      @cancel="cancel"
+    >
+      <a-form :form="form" layout="horizontal">
+        <a-form-item label="已存在数据" v-model="fakeOrderTotal">
+          {{ fakeOrderTotal }}条模拟订单数据
+        </a-form-item>
+        <a-form-item label="输入模拟姓名">
+          <a-input v-decorator="['fakename']"></a-input>
+        </a-form-item>
+        <a-form-item label="输入模拟金额">
+          <a-input-number v-decorator="['fakeprice']"></a-input-number>
+        </a-form-item>
+        <a-form-item label="是否全局使用模拟订单数据">
+          <a-switch :defaultChecked="fakeToAll" @change="onChange"></a-switch>
+        </a-form-item>
+      </a-form>
+    </a-modal>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      fakename: "",
+      fakeprice: 0,
+      fakeOrderTotal: 0,
+      fakeToAll: false,
+      form: this.$form.createForm(this),
+    };
+  },
+  props: {
+    mockVisible: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    visible() {
+      return this.mockVisible;
+    },
+  },
+  mounted() {
+    this.getFakeOrder();
+    this.getMockAll();
+  },
+  methods: {
+    // 已存在模拟数据数量
+    getFakeOrder() {
+      this.$get("/backend/notice/selectAllByNoticeStatus", {
+        noticestatus: 2,
+      }).then((res) => {
+        // console.log(res.data.data.total);
+        this.fakeOrderTotal = res.data.data.total;
+      });
+    },
+    // 查询是否全局启用
+    getMockAll() {
+      this.$get("/business/rootData").then((res) => {
+        let result = res.data.data.filter((item) => {
+          return item.id == 6;
+        });
+        this.fakeToAll = result[0].valuedata == 1 ? true : false;
+      });
+    },
+    confirm() {
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          this.$post("/backend/notice", {
+            title: values.fakename,
+            content: values.fakeprice,
+            noticestatus: 2,
+          })
+            .then(() => {
+              if (res.data.code == 200) {
+                this.$message.success("添加成功");
+                this.$emit("close");
+                this.getFakeOrder();
+                this.form.resetFields();
+              }
+            })
+            .catch(() => {
+              this.$message.error("未添加任何数据");
+              this.$emit("close");
+            });
+        }
+      });
+    },
+    cancel() {
+      this.$emit("close");
+    },
+    onChange(checked) {
+      this.fakeToAll = checked;
+      const params = {
+        id: 6,
+        valuedata: this.fakeToAll ? 1 : 0,
+      };
+      this.$post("/business/rootData/update", { ...params }).then((res) => {
+        if (res.data.code == 200) {
+          this.$message.success("切换成功");
+        }
+      });
+    },
+  },
+};
+</script>
+<style>
+</style>
